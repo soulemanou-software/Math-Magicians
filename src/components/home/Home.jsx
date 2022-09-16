4abf10a69e2f6ad78b3efaab6cc5bdefca87693e
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-09-16 11:57:51 +0100

    fixed linter errors

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -11,6 +11,10 @@ function Home() {
         nesciunt nisi recusandae rerum corrupti unde. Dolorem nulla eaque in
         quisquam, ut minus, aperiam consequatur odio laudantium iusto fugiat
         repudiandae deleniti dolores ab consectetur a mollitia eos quam natus?
+        Repudiandae officiis fuga consectetur, impedit sed eligendi aperiam,
+        velit, quidem hic ex perferendis culpa aut illum! Nisi, iste. Officia
+        veniam quis doloribus? Officiis, accusantium nesciunt.
+      </p>
     </div>
   );
 }
