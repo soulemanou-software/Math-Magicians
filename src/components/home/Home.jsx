63d4a252179b7b34332da3eb797648feeec311b3
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-09-16 11:38:14 +0100

    full wesapp complited

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,18 @@
+import styles from './Home.module.css';
+
+function Home() {
+  return (
+    <div className={styles.home}>
+      <h1>Welcome to our page!</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia deleniti
+        commodi cupiditate obcaecati inventore, repellat minus facilis eligendi
+        sed delectus, voluptates aliquam impedit, quaerat aperiam architecto
+        nesciunt nisi recusandae rerum corrupti unde. Dolorem nulla eaque in
+        quisquam, ut minus, aperiam consequatur odio laudantium iusto fugiat
+        repudiandae deleniti dolores ab consectetur a mollitia eos quam natus?
+    </div>
+  );
+}
+
+export default Home;
